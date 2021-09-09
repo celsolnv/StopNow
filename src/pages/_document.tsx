af038c70c7bc4876a3b50ea27b00c7c6144d31a8
Celso <celsolnv27@gmail.com>
2021-09-09 15:36:53 -0300

    Adicionando meta de responsividade

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -31,6 +31,7 @@ export default class MyDocument extends Document{
         return(
             <Html>
                 <Head>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                     <link rel="shortcut icon" href="favicon.png" type="image/png"/>
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
